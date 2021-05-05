f80db0420abaa44094d3d4c556a65965272f553a
pvijaykrishna549 <you@example.com>
2021-05-05 15:09:12 +0530

    UC-4: Solved using Case Statement

---
+++ EmpWageBuilderUC4.java
@@ -0,0 +1,42 @@
+package javapractice;
+
+public class EmpWageBuilderUC4 {
+	// Constants	
+			public static final int IS_FULL_TIME = 2;
+			public static final int IS_PART_TIME = 1;
+			public static final int EMP_RATE_PER_HOUR = 20;
+			
+		
+		public static void main(String args[]) {
+		
+		
+		
+		//variables
+		int empHrs = 0;
+		int empWage = 0;
+		
+	
+			
+		int empCheck = (int) Math.floor(Math.random() * 10 )% 3;
+
+	    switch (empCheck) {
+	    
+	    
+	    case IS_FULL_TIME:
+			empHrs = 8;
+			break;
+			case IS_PART_TIME:
+			empHrs = 4;
+			break;
+		     default:
+			empHrs = 0;
+	    }
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: " + empWage);
+		
+	}
+
+}
+
+
+
